dcfbf11bc4d565c45af9a1e6b263c3545038270c
Lev <loewa2011@gmail.com>
2026-05-19 17:59:51 +0200

    accounts

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.ALCES_SUPABASE = {
-  url: "https://YOUR-PROJECT-REF.supabase.co",
-  publishableKey: "YOUR-PUBLISHABLE-OR-ANON-KEY",
+  url: "https://isggnmhanhhecmustncb.supabase.co/rest/v1/",
+  publishableKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzZ2dubWhhbmhoZWNtdXN0bmNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxOTkxNDcsImV4cCI6MjA5NDc3NTE0N30.xy2ULbEwgqJKV80vvH_Ri6Cp4s6wp4UtcgJLGsMJ7ew",
 };
